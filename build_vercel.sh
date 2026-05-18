4530ad1c1b8e477897fdf5f175571d981f4dac40
rdmbtc <ntraid03@gmail.com>
2026-05-19 01:32:15 +0500

    Fix Vercel build: use shell script to generate secrets.dart

---
+++ build_vercel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Generate secrets.dart from Vercel environment variables
+printf "const supabaseUrl = '%s';\nconst supabaseAnonKey = '%s';\n" \
+  "$SUPABASE_URL" "$SUPABASE_ANON_KEY" > lib/core/secrets.dart
+
+echo "Generated secrets.dart"
+
+# Clone Flutter if not present
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git --depth 1 -b stable flutter
+fi
+
+# Build web
+flutter/bin/flutter build web --release
